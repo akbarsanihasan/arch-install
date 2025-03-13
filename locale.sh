87127e569ec0175659de5cee991341fbafa54f4d
akbarsanihasan <akbarsani.dev@gmail.com>
2025-03-13 09:59:39 +0700

    update: locale

--- locale.sh
+++ locale.sh
@@ -1,4 +1,6 @@
 setting_locale() {
+    local ADDITIONAL_LOCALE="id_ID.UTF-8"
+
     clear
     print_color "${MAGENTA}" "Setting locale and language...\n"
 
@@ -6,22 +8,24 @@ setting_locale() {
     timedatectl set-ntp true &>/dev/null
     hwclock --systohc &>/dev/null
 
-    ADDITIONAL_LOCALE="id_ID.UTF-8"
+    arch-chroot "${ROOT_MOUNTPOINT}" sed -i '/^#en_GB.UTF-8/s/^#//' /etc/locale.gen
+    arch-chroot "${ROOT_MOUNTPOINT}" sed -i '/^#en_US.UTF-8/s/^#//' /etc/locale.gen
+    arch-chroot "${ROOT_MOUNTPOINT}" sed -i "/^#$ADDITIONAL_LOCALE/s/^#//" /etc/locale.gen
 
-    sed -i '/^#en_GB.UTF-8/s/^#//' "${ROOT_MOUNTPOINT}"/etc/locale.gen
-    sed -i '/^#en_US.UTF-8/s/^#//' "${ROOT_MOUNTPOINT}"/etc/locale.gen
-    sed -i "/^#$ADDITIONAL_LOCALE/s/^#//" "${ROOT_MOUNTPOINT}"/etc/locale.gen
-
-    echo "LANG=en_GB.UTF-8" | tee "${ROOT_MOUNTPOINT}"/etc/locale.conf &>/dev/null
-    echo "LANGUAGE=en_GB.UTF-8" | tee -a "${ROOT_MOUNTPOINT}"/etc/locale.conf &>/dev/null
-    echo "LC_TIME=$ADDITIONAL_LOCALE" | tee -a "${ROOT_MOUNTPOINT}"/etc/locale.conf &>/dev/null
-    echo "LC_ADDRESS=$ADDITIONAL_LOCALE" | tee -a "${ROOT_MOUNTPOINT}"/etc/locale.conf &>/dev/null
-    echo "LC_IDENTIFICATION=$ADDITIONAL_LOCALE" | tee -a "${ROOT_MOUNTPOINT}"/etc/locale.conf &>/dev/null
-    echo "LC_TELEPHONE=$ADDITIONAL_LOCALE" | tee -a "${ROOT_MOUNTPOINT}"/etc/locale.conf &>/dev/null
-    echo "LC_PAPER=$ADDITIONAL_LOCALE" | tee -a "${ROOT_MOUNTPOINT}"/etc/locale.conf &>/dev/null
-    echo "LC_MONETARY=$ADDITIONAL_LOCALE" | tee -a "${ROOT_MOUNTPOINT}"/etc/locale.conf &>/dev/null
-    echo "LC_NUMERIC=$ADDITIONAL_LOCALE" | tee -a "${ROOT_MOUNTPOINT}"/etc/locale.conf &>/dev/null
-    echo "LC_MEASUREMENT=$ADDITIONAL_LOCALE" | tee -a "${ROOT_MOUNTPOINT}"/etc/locale.conf &>/dev/null
+    echo -e "LANG=\"en_GB.UTF-8\"" | tee ${ROOT_MOUNTPOINT}/etc/locale.conf &>/dev/null
+    echo -e "LANGUAGE=\"en_GB.UTF-8\"" | tee -a ${ROOT_MOUNTPOINT}/etc/locale.conf &>/dev/null
+    echo -e "LC_CTYPE=\"$ADDITIONAL_LOCALE\"" | tee -a ${ROOT_MOUNTPOINT}/etc/locale.conf &>/dev/null
+    echo -e "LC_COLLATE=\"$ADDITIONAL_LOCALE\"" | tee -a ${ROOT_MOUNTPOINT}/etc/locale.conf &>/dev/null
+    echo -e "LC_MESSAGES=\"$ADDITIONAL_LOCALE\"" | tee -a ${ROOT_MOUNTPOINT}/etc/locale.conf &>/dev/null
+    echo -e "LC_NAME=\"$ADDITIONAL_LOCALE\"" | tee -a ${ROOT_MOUNTPOINT}/etc/locale.conf &>/dev/null
+    echo -e "LC_NUMERIC=\"$ADDITIONAL_LOCALE\"" | tee -a ${ROOT_MOUNTPOINT}/etc/locale.conf &>/dev/null
+    echo -e "LC_TIME=\"$ADDITIONAL_LOCALE\"" | tee -a ${ROOT_MOUNTPOINT}/etc/locale.conf &>/dev/null
+    echo -e "LC_MONETARY=\"$ADDITIONAL_LOCALE\"" | tee -a ${ROOT_MOUNTPOINT}/etc/locale.conf &>/dev/null
+    echo -e "LC_PAPER=\"$ADDITIONAL_LOCALE\"" | tee -a ${ROOT_MOUNTPOINT}/etc/locale.conf &>/dev/null
+    echo -e "LC_ADDRESS=\"$ADDITIONAL_LOCALE\"" | tee -a ${ROOT_MOUNTPOINT}/etc/locale.conf &>/dev/null
+    echo -e "LC_TELEPHONE=\"$ADDITIONAL_LOCALE\"" | tee -a ${ROOT_MOUNTPOINT}/etc/locale.conf &>/dev/null
+    echo -e "LC_MEASUREMENT=\"$ADDITIONAL_LOCALE\"" | tee -a ${ROOT_MOUNTPOINT}/etc/locale.conf &>/dev/null
+    echo -e "LC_IDENTIFICATION=\"$ADDITIONAL_LOCALE\"" | tee -a ${ROOT_MOUNTPOINT}/etc/locale.conf &>/dev/null
 
     arch-chroot "${ROOT_MOUNTPOINT}" locale-gen
 
