05b80cd350a47debc5f2f531a18dd43145c88018
akbarsanihasan <akbarsani.dev@gmail.com>
2025-04-03 15:35:33 +0700

    fix: fstab

--- disk.sh
+++ disk.sh
@@ -5,7 +5,7 @@ setting_storage() {
     echo -e "# <file system> <dir> <type> <options> <dump> <pass>" |
         tee "${ROOT_MOUNTPOINT}"/etc/fstab &>/dev/null
 
-    echo -e "UUID=$(get_partinfo "UUID" $EFI_PARTITION)     ${ESP_MOUNTPOINT#${ROOT_PARTITION}}       $(get_partinfo "type" $EFI_PARTITION)      umask=0077      0       1" |
+    echo -e "UUID=$(get_partinfo "UUID" $EFI_PARTITION)     ${ESP_MOUNTPOINT}#${ROOT_MOUNTPOINT}       $(get_partinfo "type" $EFI_PARTITION)      umask=0077      0       1" |
         tee -a "${ROOT_MOUNTPOINT}"/etc/fstab &>/dev/null
 
     echo -e "UUID=$(get_partinfo "UUID" $ROOT_PARTITION)     /       $(get_partinfo "type" $ROOT_PARTITION)      errors=remount-ro      0       1" |
@@ -13,6 +13,7 @@ setting_storage() {
 
     if [[ "${#EXTRA_STORAGE[@]}" -gt 0 ]]; then
         for i in "${!EXTRA_STORAGE[@]}"; do
+            mkdir -p ${EXTRA_STORAGE_MOUNTPOINT[$i]}
             echo -e "$(get_partinfo "UUID" ${EXTRA_STORAGE[$i]})     ${EXTRA_STORAGE_MOUNTPOINT[$i]}       $(get_partinfo "type" ${EXTRA_STORAGE[$i]})      defaults,uid=$(arch-chroot ${ROOT_MOUNTPOINT} id -u $USERNAME),gid=$(arch-chroot ${ROOT_MOUNTPOINT} id -g $USERNAME),nofail      0       0" |
                 tee -a "${ROOT_MOUNTPOINT}"/etc/fstab &>/dev/null
         done
