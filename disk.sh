1df05e0dfc8675bc0314ecea523b1ce488bf503e
Akbar Sani Hasan <114196216+akbarsanihasan@users.noreply.github.com>
2025-04-17 15:37:55 +0700

    Update: disk.sh

--- disk.sh
+++ disk.sh
@@ -23,11 +23,13 @@ setting_storage() {
 		fi
 
 		if ! [[ -z $extra_type ]]; then
+    mkdir -p "$ROOT_MOUNTPOINT"/"$extra_mountpoint"
 			case "$extra_type" in
 			ntfs | exfat)
 				echo -e "UUID=$extra_uuid $extra_mountpoint $extra_fstype defaults,uid=$uid,gid=$gid,nofail 0 0" | tee -a "$ROOT_MOUNTPOINT"/etc/fstab
 				;;
 			ext4)
+    arch-chroot "$ROOT_MOUNTPOINT" chown -R "$USERNAME" $extra_mountpoint
 				echo -e "UUID=$extra_uuid $extra_mountpoint $extra_fstype defaults,nofail 0 0" | tee -a "$ROOT_MOUNTPOINT"/etc/fstab
 				;;
 			esac
