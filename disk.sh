5699da207539de88ed916565fa60890b87cae823
Akbar Sani Hasan <114196216+akbarsanihasan@users.noreply.github.com>
2025-04-17 20:12:51 +0700

    Update disk.sh

--- disk.sh
+++ disk.sh
@@ -18,6 +18,9 @@ setting_storage() {
 		local extra_uuid=$(get_partinfo "uuid" "$extra_disk_part")
 		local extra_label=$(get_partinfo "label" "$extra_disk_part")
 		local extra_mountpoint="/media/$extra_label"
+                local uid=$(arch-chroot "$ROOT_MOUNTPOINT" id -u $USERNAME)
+		local gid=$(arch-chroot "$ROOT_MOUNTPOINT" id -g $USERNAME)
+  
 		if [[ -z $extra_label ]]; then
 			extra_mountpoint="/media/$extra_uuid"
 		fi
@@ -26,11 +29,11 @@ setting_storage() {
                         mkdir -p "$ROOT_MOUNTPOINT"/"$extra_mountpoint"
 			case "$extra_type" in
 			ntfs | exfat)
-				echo -e "UUID=$extra_uuid $extra_mountpoint $extra_fstype defaults,uid=$uid,gid=$gid,nofail 0 0" | tee -a "$ROOT_MOUNTPOINT"/etc/fstab
+				echo -e "UUID=$extra_uuid $extra_mountpoint $extra_type defaults,uid=$uid,gid=$gid,nofail 0 0" | tee -a "$ROOT_MOUNTPOINT"/etc/fstab
 				;;
 			ext4)
                                 arch-chroot "$ROOT_MOUNTPOINT" chown -R "$USERNAME" $extra_mountpoint
-				echo -e "UUID=$extra_uuid $extra_mountpoint $extra_fstype defaults,nofail 0 0" | tee -a "$ROOT_MOUNTPOINT"/etc/fstab
+				echo -e "UUID=$extra_uuid $extra_mountpoint $extra_type defaults,nofail 0 0" | tee -a "$ROOT_MOUNTPOINT"/etc/fstab
 				;;
 			esac
 
