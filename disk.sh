0e0bdce6d963fa202bc6b86e96a3beb32f4c6198
akbarsanihasan <akbarsani.dev@gmail.com>
2025-04-18 07:26:35 +0700

    update: fstab generation

--- disk.sh
+++ disk.sh
@@ -1,8 +1,22 @@
+is_usb_device() {
+	local dev="$1"
+
+	if [[ "$dev" =~ ^nvme[0-9]+n[0-9]+p[0-9]+$ ]]; then
+		dev="${dev%p[0-9]*}"
+	else
+		dev="${dev%%[0-9]*}"
+	fi
+
+	udevadm info --query=property --name="/dev/$dev" | grep -q '^ID_BUS=usb'
+
+	return $?
+}
+
 setting_storage() {
 	clear
 	print_color "$MAGENTA" "Configuring fstab... \n"
 
-	disks=($(lsblk -p -l -n -f -o name))
+	disks=("$(lsblk -p -l -n -f -o name)")
 	esp_uuid=$(get_partinfo "uuid" "$EFI_PARTITION")
 	esp_type=$(get_partinfo "type" "$EFI_PARTITION")
 
@@ -18,25 +32,32 @@ setting_storage() {
 		local extra_uuid=$(get_partinfo "uuid" "$extra_disk_part")
 		local extra_label=$(get_partinfo "label" "$extra_disk_part")
 		local extra_mountpoint="/media/$extra_label"
-                local uid=$(arch-chroot "$ROOT_MOUNTPOINT" id -u $USERNAME)
-		local gid=$(arch-chroot "$ROOT_MOUNTPOINT" id -g $USERNAME)
-  
+		local uid=$(arch-chroot "$ROOT_MOUNTPOINT" id -u "$USERNAME")
+		local gid=$(arch-chroot "$ROOT_MOUNTPOINT" id -g "$USERNAME")
+
 		if [[ -z $extra_label ]]; then
 			extra_mountpoint="/media/$extra_uuid"
 		fi
 
-		if ! [[ -z $extra_type ]]; then
-                        mkdir -p "$ROOT_MOUNTPOINT"/"$extra_mountpoint"
+		if [[ -n "$extra_type" ]] &&
+			[[ "$extra_disk_part" != "$EFI_PARTITION" ]] &&
+			[[ "$extra_disk_part" != "$EFI_PARTITION" ]] &&
+			[[ "$extra_disk_part" != "$ROOT_PARTITION" ]] &&
+			! [[ "$extra_disk_part" =~ [0-9]$ ]] &&
+			! [[ "$extra_disk_part" =~ ^nvme[0-9]+n[0-9]+p[0-9]+$ ]] &&
+			! is_usb_device "$extra_disk_part"; then
+
+			mkdir -p "$ROOT_MOUNTPOINT"/"$extra_mountpoint"
+
 			case "$extra_type" in
 			ntfs | exfat)
 				echo -e "UUID=$extra_uuid $extra_mountpoint $extra_type defaults,uid=$uid,gid=$gid,nofail 0 0" | tee -a "$ROOT_MOUNTPOINT"/etc/fstab
 				;;
 			ext4)
-                                arch-chroot "$ROOT_MOUNTPOINT" chown -R "$USERNAME" $extra_mountpoint
+				arch-chroot "$ROOT_MOUNTPOINT" chown -R "$USERNAME" "$extra_mountpoint"
 				echo -e "UUID=$extra_uuid $extra_mountpoint $extra_type defaults,nofail 0 0" | tee -a "$ROOT_MOUNTPOINT"/etc/fstab
 				;;
 			esac
-
 		fi
 	done
 
